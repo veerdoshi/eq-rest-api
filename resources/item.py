4225cc4e8b2edfc0ee053ca27677e35bfed19dd4
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-21 14:45:59 -0800

    Update item.py

--- resources/item.py
+++ resources/item.py
@@ -11,8 +11,8 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, measure):
-       quakeitem = ItemModel.find_by_measure(measure)
-       if quakeitem:
+        quakeitem = ItemModel.find_by_measure(measure)
+        if quakeitem:
             return quakeitem.json()
         return {'message': 'Quake measure not found'}, 404
 
