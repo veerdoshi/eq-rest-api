7c148c13245ea1f9840cd34969f0e7547bac1744
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-22 13:29:27 -0800

    Update quake.py

--- models/quake.py
+++ models/quake.py
@@ -1,7 +1,7 @@
 from db import db
 
 class QuakeModel(db.Model):
-    __tablename__ = 'items'
+    __tablename__ = 'quakes'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
