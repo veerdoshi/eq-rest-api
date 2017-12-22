56c9de93bed3d0f978c6105be0148b4bee74024a
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-22 11:57:02 -0800

    Add files via upload

---
+++ resources/quake.py
@@ -0,0 +1,60 @@
+from flask_restful import Resource, reqparse
+from models.quake import QuakeModel
+
+class Quake(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('magnitude',
+        type=float,
+        required=True,
+        help='This field cannot be left blank!'
+    )
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('magnitude',
+        type=float,
+        required=True,
+        help='This field cannot be left blank!'
+    )
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('latitude',
+        type=float,
+        required=True,
+        help='This field cannot be left blank!'
+    )
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('longitude',
+        type=float,
+        required=True,
+        help='This field cannot be left blank!'
+    )
+
+    parser.add_argument('sensor_id',
+        type=int,
+        required=True,
+        help='Every measure needs a sensor id.'
+    )
+
+#    @jwt_required()
+#    def get(self, name):
+#        item = ItemModel.find_by_name(name)
+#        if item:
+#            return item.json()
+#        return {'message': 'Item not found'}, 404
+
+
+    def post(self):
+        data = Item.parser.parse_args()
+        quake = QuakeModel(**data)
+        try:
+            quake.save_to_db()
+        except:
+            return {"message": "An error occurred while inserting the quake measure"}, 500
+
+        return quake.json(), 201
+
+
+class ItemList(Resource):
+    def get(self):
+         return {'quakes': [quake.json() for quake in QuakeModel.query.all()]}
---
+++ resources/sensor.py
@@ -0,0 +1,31 @@
+from flask_restful import Resource
+from models.store import SensorModel
+
+
+class Sensor(Resource):
+    def get(self, name):
+        sensor = SensorModel.find_by_name(name)
+        if sensor:
+            return sensor.json()
+        return {'message': 'Sensor not found'}, 404
+    def post(self, name):
+        if SensorModel.find_by_name(name):
+            return {'message': "A sensor with name '{}' already exists.".format(name)}, 400
+        sensor = SensorModel(magnitude)
+        try:
+            sensor.save_to_db()
+        except:
+            return {'message': 'An error occurred while creating the sensor.'}, 500
+
+        return sensor.json(), 201
+
+    def delete(self, name):
+        sensor = SensorModel.find_by_name(name)
+        if sensor:
+            sensor.delete_from_db()
+
+            return {'message': 'Sensor deleted'}
+
+class SensorList(Resource):
+    def get(self):
+        return {'sensors': [sensor.json() for sensor in SensorModel.query.all()]}
