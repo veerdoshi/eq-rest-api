38aa98b67b552ff4ad5540f7cb7a6587a789cb6b
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-22 12:01:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
+
 from flask import Flask
 from flask_restful import Api
-from resources.item import Item, ItemList
+
+from resources.quake import Quake, QuakeList
+from resources.sensor import Sensor, SensorList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -9,10 +12,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'veer'
 api = Api(app)
 
-api.add_resource(Item, '/quake')
 
-api.add_resource(ItemList, '/quakes')
+api.add_resource(Sensor, '/sensor/<string:name>')
+api.add_resource(Quake, '/quake/')
 
+api.add_resource(QuakeList, '/quakes')
+api.add_resource(SensorList, '/sensors')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
