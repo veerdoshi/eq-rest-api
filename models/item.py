fc8a105b933baccfe8a8d2cfed570fefe8ed5473
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2017-12-21 23:16:15 -0800

    Update item.py

--- models/item.py
+++ models/item.py
@@ -4,7 +4,7 @@ class ItemModel(db.Model):
     __tablename__ = 'quakeitems'
 
     magnitude = db.Column(db.Float(precision=1))
-    measure = db.Column(db.Integer, primary_key=True)
+#    measure = db.Column(db.Integer, primary_key=True)
 
 #    location = db.Column(db.String(80), db.ForeignKey('locations.id'))
 #    locations = db.relationship('StoreModel')
@@ -19,7 +19,7 @@ class ItemModel(db.Model):
 #    def __init__(self, name, price, store_id):
     def __init__(self, magnitude):
         self.magnitude = magnitude
-        self.measure = measure
+#        self.measure = measure
 
 #        self.location = location
 #        self.location = location
@@ -29,11 +29,11 @@ class ItemModel(db.Model):
 #        self.store_id = store_id
     def json(self):
 #        return {'name': self.name, 'price': self.price}
-        return {'measure': self.measure, 'magnitude': self.magnitude}
+        return {'magnitude': self.magnitude}
 
     @classmethod
-    def find_by_measure(cls, name):
-        return cls.query.filter_by(measure=measure).first()
+    def find_by_measure(cls, magnitude):
+       return cls.query.filter_by(magnitude[-1]).first()
 
     def save_to_db(self):
         db.session.add(self)
